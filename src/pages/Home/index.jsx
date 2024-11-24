1abcbe56972343d3b9af9a29c7d7479237bcc424
Henrique Fornazeiro <henrique.fornazeiro@hotmail.com>
2024-11-24 15:30:46 -0300

    style: css do formulario da home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,23 +3,44 @@ import { FaRegTrashAlt } from "react-icons/fa";
 
 function Home() {
 
+  const usuarios = [
+    {
+      id: '123423sdf332d3kl23jh4323',
+      name: 'Henrique',
+      age: 26,
+      email: 'henrique@email.com'
+    },
+    {
+      id: 'hfj23h4j23dfsd902d',
+      name: 'Teste',
+      age: 33,
+      email: 'teste@email.com'
+    }
+  ]
+
   return (
     <div className='container'>
       <form>
         <h1>Cadastro de usuários</h1>
-        <input name='nome' type='text'/>
-        <input name='idade' type='number'/>
-        <input name='email' type='email'/>
+        <input name='nome' type='text' placeholder='Nome'/>
+        <input name='idade' type='number' placeholder='Idade'/>
+        <input name='email' type='email' placeholder='E-mail'/>
         <button type='button'>Cadastrar</button>
       </form>
-      <div>
-      <div>
-        <p>Nome:</p>
-        <p>Idade:</p>
-        <p>Email:</p>
-      </div>
-      <button><FaRegTrashAlt /></button>
-      </div>
+      {
+        usuarios.map(usuario => (
+          <div key={usuario.id} className='userCard'>
+            <div>
+              <p>Nome: <span>{usuario.name}</span></p>
+              <p>Idade: <span>{usuario.age}</span></p>
+              <p>Email: <span>{usuario.email}</span></p>
+            </div>
+            <button><FaRegTrashAlt color='red' size={20}/></button>
+          </div>
+        )
+        )
+      }
+
     </div>
   )
 }
